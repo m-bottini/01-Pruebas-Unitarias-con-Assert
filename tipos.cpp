2748f03b4329b61648121d732b7c7b2ab949bdf1
Miguel A. Bottini <103381194+m-bottini@users.noreply.github.com>
2022-05-18 22:02:02 -0300

    Add files via upload

---
+++ tipos.cpp
@@ -0,0 +1,16 @@
+#include<cassert>
+int main () {
+    assert(2==1+1);
+    //bool
+    assert(true);
+    assert(false==false);
+    assert(false!=true);
+    assert(not false);
+    assert(false==not true);
+    assert(true and true);
+    assert(false or true);
+    assert((false and false)==false);
+    //double 
+    assert(2.0==1.0+1.0);
+   // assert(1.0==0.1+10);
+}
